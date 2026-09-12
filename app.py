6c03ccc0d20044b5fbcbf4e326ae78d2de484e63
Killi Pardhasaradhi <pardhukilli273@gmail.com>
2026-09-12 11:45:07 +0530

    Fix indentation for email sending code

--- app.py
+++ app.py
@@ -210,8 +210,8 @@ def send_otp_email(target_email, otp_code):
         msg['From'] = SENDER_EMAIL
         msg['To'] = target_email
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.login(SENDER_EMAIL, SENDER_PASSWORD)
-        server.sendmail(SENDER_EMAIL, target_email, msg.as_string())
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.sendmail(SENDER_EMAIL, target_email, msg.as_string())
         server.quit()
         return True
     except Exception as e:
