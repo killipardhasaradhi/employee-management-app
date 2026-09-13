4279f2a42c884d34b48a83d8a65167c2b567462b
Killi Pardhasaradhi <pardhukilli273@gmail.com>
2026-09-13 18:07:45 +0530

    Change app title from 'LockIn PS' to 'PS DIGITAL'

--- app.py
+++ app.py
@@ -828,7 +828,7 @@ else:
         st.markdown("""
             <div class="app-brand-header">
                 <div>
-                    <div class="app-title">LockIn PS</div>
+                    <div class="app-title">PS DIGITAL</div>
                     <div class="app-subtitle">ACCOUNT ONBOARDING</div>
                 </div>
             </div>
